d8569dfcfbca05241097a312625817e2c1c337df
codewuren <codewuren@gmail.com>
2023-11-13 19:14:13 +0800

    Add lsp-mode & company

---
+++ etc/plugins/init-lsp.el
@@ -0,0 +1,23 @@
+(use-package lsp-mode
+  :ensure t
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (c++-mode . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+(use-package company
+  :ensure t
+  :config
+  (add-to-list 'company-backends 'company-capf)
+  (global-company-mode))
+
+
+(setq gc-cons-threshold (* 100 1024 1024)
+      read-process-output-max (* 1024 1024)
+      treemacs-space-between-root-nodes nil
+      company-idle-delay 0.0
+      company-minimum-prefix-length 1
+      lsp-idle-delay 0.1)  ;; clangd is fast
+
+(provide 'init-lsp)
--- etc/plugins/init-plugins.el
+++ etc/plugins/init-plugins.el
@@ -1,5 +1,7 @@
 ; Requires
 (require 'init-which-key)
+(require 'init-lsp)
+(require 'init-cpp)
 (require 'init-awesome-tab)
 
 (provide 'init-plugins)
